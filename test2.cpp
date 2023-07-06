e41a1b26a7a78c2be7eb37ca6d16c2b50ff2b94f
kantakei <82003300+kantakei@users.noreply.github.com>
2023-07-06 15:58:36 +0900

    Update test2.cpp

--- test2.cpp
+++ test2.cpp
@@ -9,7 +9,7 @@ DigitalOut SN74LV595_CLK(p11);
 DigitalIn  SN74LV595_DOUT(p10);
 AnalogIn ain(p20);
 
-RawSerial pc(USBTX, USBRX);
+Serial pc(USBTX, USBRX);
 
 SPI spiDAAD(p5, p6, p7);
 DigitalOut DA_sync(p8);
@@ -49,7 +49,7 @@ void SN74LV595FastScan(int usWhichPin)
     static int pos;
 
     SN74LV595_RCLK = 0;
-    if (usWhichPin == 0) { //set 01�iHigh)
+    if (usWhichPin == 0) {
         SN74LV595_DIN = 0;
         SN74LV595_CLK = 1;
         SN74LV595_CLK = 0;
@@ -59,7 +59,7 @@ void SN74LV595FastScan(int usWhichPin)
         pos = 0;
     } else {
         pin = usWhichPin - pos;
-        for (ii = 0; ii < pin; ii++) {//set 10 (GND)
+        for (ii = 0; ii < pin; ii++) {
             SN74LV595_DIN = 1;
             SN74LV595_CLK = 1;
             SN74LV595_CLK = 0;
@@ -69,7 +69,6 @@ void SN74LV595FastScan(int usWhichPin)
         }
         pos = usWhichPin;
     }
-    //Load S/R
     SN74LV595_RCLK = 1;
     SN74LV595_RCLK = 0;
 }
@@ -87,7 +86,6 @@ void SN74LV595AllScan()
         SN74LV595_CLK = 1;
         SN74LV595_CLK = 0;
     }
-    //Load S/R
     SN74LV595_RCLK = 1;
     SN74LV595_RCLK = 0;
 }
@@ -117,7 +115,6 @@ void SN74LV595Init(int TotalPin)
         SN74LV595_CLK = 1;
         SN74LV595_CLK = 0;
     }
-    //Load S/R
     SN74LV595_RCLK = 1;
     SN74LV595_RCLK = 0;
 }
@@ -126,21 +123,19 @@ short DAAD(short DA)
 {
     short AD;
 
-    //enable
     DA_sync = 0;
     AD_cs = 0;
-    //simultaneous DA and AD
+
     AD = spiDAAD.write(DA << 2);
-    //disable
+
     DA_sync = 1;
     AD_cs = 1;
 
-    return AD >> 2;//bottom 2bits are unnecessary
+    return AD >> 2;
 }
 
 void DAADinit()
 {
-    //Setup SPI, 16bit, falling edge, 48MHz clock
     spiDAAD.format(16, 2);
     spiDAAD.frequency(48000000);
 }
@@ -148,11 +143,11 @@ void DAADinit()
 int SerialLeap()
 {
     speed = pc.getc();
-    pc.putc(speed);
+    // pc.putc(speed);
     return speed;
 }
 
-void SerialReceiveInterrupt()
+void SerialReceive()
 {
     int i;
     unsigned char data[255];
@@ -161,7 +156,6 @@ void SerialReceiveInterrupt()
     int rcv = pc.getc() - 256;
 
     if (rcv >= -14 && rcv <= 0) {
-        // pc.putc(rcv);
         if (rcv == DA_TEST) {
             Mode = DA_TEST;
             myleds = 1;
@@ -187,24 +181,24 @@ void SerialReceiveInterrupt()
             delta_oldflag = false;
             Mode = organdy;
             myleds = 2;
-        } else if(rcv == -9) {
+        } else if (rcv == -9) {
             amp -= 10.0;
-            if(amp <0.0)
+            if (amp <0.0)
                 amp = 0.0;
             myleds = 4;
-        } else if(rcv == -10) {
+        } else if (rcv == -10) {
             amp += 10.0;
-            if(amp > 500.0)
+            if (amp > 500.0)
                 amp = 500.0;
             myleds = 4;
-        } else if(rcv == -11) {
+        } else if (rcv == -11) {
             freq += 5.0;
-            if(freq > 500.0)
+            if (freq > 500.0)
                 freq = 500.0;
             myleds = 4;
-        } else if(rcv == -12) {
+        } else if (rcv == -12) {
             freq -= 5.0;
-            if(freq < 0.0)
+            if (freq < 0.0)
                 freq = 0.0;
             myleds = 4;
         } 
@@ -221,13 +215,11 @@ int main()
     int i;
     short AD;
 
-    float cntrspeed = 0.0;
-
     int prevmode;
 
     DAADinit();
     SN74LV595Init(ELECTRODE_NUM);
-    pc.attach(SerialReceiveInterrupt);
+    pc.attach(SerialReceive);
     pc.baud(921600);
     timer.start();
 
@@ -238,68 +230,66 @@ int main()
 
     myleds = 1;
 
-    // cntrspeed = SerialLeap();
-
     while (1) {
         if (Mode == DA_TEST) {
             t = (double)timer.read_us() * 0.000001;
 			AD = DAAD((short)(200.0 * (1.0 + sin(2.0 * 3.1415926 * freq * t))));
         } else if (Mode == pile) {
             t = (double)timer.read_us() * 0.000001;
-            AD = DAAD((short)(amp * (1.0 + sin(2.0 * 3.1415926 * freq * t))));
+            AD = DAAD((short)(amp * (1.0 + sin(2.0 * 3.1415926 * freq * t * speed))));
             prevmode = Mode;
         } else if (Mode == doby) {
             t = (double)timer.read_us() * 0.000001;
-            int d = (int)(t * 100 * freq) % 100;
+            int d = (int)(t * 100 * freq * speed) % 100;
             double dd = (double)d / 100;
             dd *= 2 * amp;
-            if(dd > 600)
+            if (dd > 600)
                 dd = 600;
             AD = DAAD((short)dd);
             prevmode = Mode;
         } else if (Mode == satin) {
             t = (double)timer.read_us() * 0.000001;
-            AD = DAAD((short)(amp * (1.0 + sin(2.0 * 3.1415926 * freq * t)))); 
+            AD = DAAD((short)(amp * (1.0 + sin(2.0 * 3.1415926 * freq * t * speed)))); 
             prevmode = Mode;
         } else if (Mode == fleece) {
             t = (double)timer.read_us() * 0.000001;
-            if(sin(2.0 * 3.1415926 * freq * t) > 0.0)
+            if (sin(2.0 * 3.1415926 * freq * t * speed) > 0.0)
                 AD = DAAD((short)amp * 2);
             else
                 AD = DAAD((short)0);
             prevmode = Mode;
         } else if (Mode == microfiber) {
             t = (double)timer.read_us() * 0.000001;
-            if(sin(2.0 * 3.1415926 * freq * t) > 0.995)
+            if (sin(2.0 * 3.1415926 * freq * t * speed) > 0.995)
                 delta_flag = true;
             else
                 delta_flag = false;
-            if(delta_cnt == 8) {
+            if (delta_cnt == 8) {
                 delta_cnt = 0;
                 AD = DAAD((short)0);
             }
-            if(delta_cnt == 7) {
+            if (delta_cnt == 7) {
                 delta_cnt = 8;
             }
-            if(delta_cnt == 6) {
+            if (delta_cnt == 6) {
                 delta_cnt = 7;
             }
-            if(delta_cnt == 5) {
+            if (delta_cnt == 5) {
                 delta_cnt = 6;
             }
-            if(delta_cnt == 4) {
+            if (delta_cnt == 4) {
                 delta_cnt = 5;
             }
-            if(delta_cnt == 3) {
+            if (delta_cnt == 3) {
                 delta_cnt = 4;
             }
-            if(delta_cnt == 2) {
+            if (delta_cnt == 2) {
                 delta_cnt = 3;
             }
-            if(delta_cnt == 1) {
+            if (delta_cnt == 1) {
                 delta_cnt = 2;
             }
-            if(delta_oldflag == false && delta_flag == true) {
+            if (delta_oldflag == false && delta_flag == true) {
                 delta_cnt = 1;
                 AD = DAAD((short) amp * 2);
             }
@@ -307,36 +297,36 @@ int main()
             prevmode = Mode;
         } else if (Mode == organdy) {
             t = (double)timer.read_us() * 0.000001;
-            if(sin(2.0 * 3.1415926 * freq * t) > 0.995) 
+            if (sin(2.0 * 3.1415926 * freq * t * speed) > 0.995) 
                 delta_flag = true;
             else
                 delta_flag = false;
-            if(delta_cnt == 8) {
+            if (delta_cnt == 8) {
                 delta_cnt = 0;
                 AD = DAAD((short)0);
             }
-            if(delta_cnt == 7) {
+            if (delta_cnt == 7) {
                 delta_cnt = 8;
             }
-            if(delta_cnt == 6) {
+            if (delta_cnt == 6) {
                 delta_cnt = 7;
             }
-            if(delta_cnt == 5) {
+            if (delta_cnt == 5) {
                 delta_cnt = 6;
             }
-            if(delta_cnt == 4) {
+            if (delta_cnt == 4) {
                 delta_cnt = 5;
             }
-            if(delta_cnt == 3) {
+            if (delta_cnt == 3) {
                 delta_cnt = 4;
             }
-            if(delta_cnt == 2) {
+            if (delta_cnt == 2) {
                 delta_cnt = 3;
             }
-            if(delta_cnt == 1) {
+            if (delta_cnt == 1) {
                 delta_cnt = 2;
             }
-            if(delta_oldflag == false && delta_flag == true) {
+            if (delta_oldflag == false && delta_flag == true) {
                 delta_cnt = 1;
                 AD = DAAD((short) amp * 2);
             }
